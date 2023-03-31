64d38cdbb7dfac7135e78bb603c592776beeef4f
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-04-01 00:07:00 +0100

    progress

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,6 +3,19 @@ import type { PageServerLoad } from './$types';
 import { request } from 'undici';
 import { CLIENT_ID, CLIENT_SECRET } from '$env/static/private';
 
+async function fetchDiscordProfile(accessToken: string) {
+    const { body, statusCode } = await request('https://discord.com/api/users/@me', {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    });
+
+    if (statusCode !== 200) {
+        throw new Error('Failed to fetch Discord user profile');
+    }
+
+    return body.json();
+}
  
 export const load: PageServerLoad = async ({ url }) => {
     let code = url.searchParams.get("code");
@@ -29,7 +42,7 @@ export const load: PageServerLoad = async ({ url }) => {
             // NOTE: An unauthorized token will not throw an error
             // tokenResponseData.statusCode will be 401
             console.error(error);
-        }console.log("HI");
+        }
     } else {
         return false;
     }
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,11 +7,100 @@
 
     export let data: PageData;
     
-    console.log(data);
+    let isLoginForm = true;
+
+
+  // Handle form submission for login
+  function handleLoginSubmit(event: Event) {
+    event.preventDefault();
+    // TODO: Handle login form submission
+  }
+
+  // Handle form submission for sign-up
+  function handleSignupSubmit(event: Event) {
+    event.preventDefault();
+    // TODO: Handle sign-up form submission
+  }
+
+  function toggleLoginForm() {
+    isLoginForm = !isLoginForm;
+  }
 </script>
 
 <div class="grid h-[80vh] place-items-center">
-   <a href="https://discord.com/api/oauth2/authorize?client_id=1015009145482649760&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&response_type=code&scope=identify" class="bg-[#635def] px-5 py-3 rounded-md hover:bg-[#433fa2] transition">
-        <span class="text-white font-semibold text-xl"><i class="fa-brands fa-discord mr-2"></i> Login with Discord</span>
-    </a> 
+    {#if isLoginForm}
+    <!-- Login Form -->
+    <form on:submit={handleLoginSubmit} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+            <h1 class="text-3xl font-bold mb-4">Sign in</h1>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="username">
+                Username or Email
+            </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username or Email" />
+        </div>
+        <div class="mb-6">
+        <label class="block text-gray-700 font-bold mb-2" for="password">
+            Password
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
+        </div>
+        <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Sign In
+        </button>
+        <span class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 cursor-pointer" on:click={toggleLoginForm}>
+            Create an account
+        </span>
+        </div>
+        <div class="mt-4">
+        <hr class="border-gray-400">
+        <p class="text-center my-4 font-bold">OR</p>
+        <a href="https://discord.com/api/oauth2/authorize?client_id=1015009145482649760&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&response_type=code&scope=identify" class="bg-[#635def] px-5 py-3 rounded-md hover:bg-[#433fa2] transition block w-full">
+            <span class="text-white font-semibold text-xl"><i class="fa-brands fa-discord mr-2"></i> Sign in with Discord</span>
+        </a>
+        </div>
+    </form>
+    {:else}
+        <!-- Sign-up Form -->
+        <form on:submit={handleSignupSubmit} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+       <div class="mb-4">
+        <h1 class="text-3xl font-bold mb-4">Create an account</h1>
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="username">
+          Username
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="email">
+          Email
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 font-bold mb-2" for="password">
+          Password
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Sign Up
+        </button>
+        <span class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 cursor-pointer" on:click={toggleLoginForm}>
+          Sign In
+        </span>
+      </div>
+      <div class="mt-4">
+        <hr class="border-gray-400">
+        <p class="text-center my-4 font-bold">OR</p>
+        <a href="https://discord.com/api/oauth2/authorize?client_id=1015009145482649760&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&response_type=code&scope=identify" class="bg-[#635def] px-5 py-3 rounded-md hover:bg-[#433fa2] transition block w-full">
+          <span class="text-white font-semibold text-xl"><i class="fa-brands fa-discord mr-2"></i> Sign up with Discord</span>
+        </a>
+      </div>
+    </form>
+  {/if}
 </div>
